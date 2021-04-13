8cd6e9a81f1d6e325b94141daa22f22ab6a8fdb1
Seth W <swidoff@gmail.com>
2021-04-13 12:09:18 -0700

    day 23 checkpoint.

--- src/day23.py
+++ src/day23.py
@@ -1,12 +1,8 @@
 from __future__ import annotations
 
-import dataclasses
-import math
-import operator
-from dataclasses import dataclass
-from functools import cached_property
-from typing import List, Tuple
 import re
+from dataclasses import dataclass
+from typing import List, Tuple
 
 
 def read_input() -> List[str]:
@@ -26,22 +22,25 @@ class Nanobot(object):
 
     def is_cube_in_range(self, x1: int, x2: int, y1: int, y2: int, z1: int, z2: int) -> bool:
         # Cube is in range if:
+
         # 1. One of the cube corners is in range
-        cube_corners_in_range = any(
+        cube_points_in_range = any(
             self.is_point_in_range(x, y, z)
-            for x in (x1, x2)
-            for y in (y1, y2)
-            for z in (z1, z2)
+            for x in (x1, (x2 + x1 // 2), x2)
+            for y in (y1, (y2 + y1 // 2), y2)
+            for z in (z1, (z2 + z1 // 2), z2)
         )
 
         # 2. Any of the radius endpoints are in the cube.
         endpoints_in_cube = any(
-            x1 <= self.x + x_d * sign <= x2 and y1 <= self.y + y_d * sign <= y2 and z1 <= self.z + z_d * sign <= z2
+            x1 <= self.x + x_d * sign * self.radius <= x2
+            and y1 <= self.y + y_d * sign * self.radius <= y2
+            and z1 <= self.z + z_d * sign * self.radius <= z2
             for sign in (-1, 1)
             for x_d, y_d, z_d in ((1, 0, 0), (0, 1, 0), (0, 0, 1))
         )
 
-        return cube_corners_in_range or endpoints_in_cube
+        return cube_points_in_range or endpoints_in_cube
 
 
 def parse_input(lines: List[str]) -> List[Nanobot]:
@@ -107,6 +106,8 @@ def part2(nanobots: List[Nanobot]):
     max_y = max(n.y + n.radius for n in nanobots)
     min_z = min(n.z - n.radius for n in nanobots)
     max_z = max(n.z + n.radius for n in nanobots)
+    point = (min_x, min_y, min_z)
+    dim = max((max_x - min_x, max_y - min_y, max_z - min_z))
 
     last_best = None
     while True:
@@ -136,7 +137,7 @@ def part2(nanobots: List[Nanobot]):
                     regions.append((x1, x2, y1, y2, z1, z2, len(bots)))
 
         max_bots = max(r[-1] for r in regions)
-        best_regions =[r for r in regions if r[-1] == max_bots]
+        best_regions = [r for r in regions if r[-1] == max_bots]
         if len(best_regions) > 1:
             best_region = min(best_regions, key=lambda r: abs(r[0]) + abs(r[2]) + abs(r[4]))
         else:
@@ -165,7 +166,6 @@ def part2(nanobots: List[Nanobot]):
     return dist
 
 
-
 def test_part2_examples():
     example = """pos=<10,12,12>, r=2
 pos=<12,14,12>, r=2
@@ -174,9 +174,6 @@ pos=<14,14,14>, r=6
 pos=<50,50,50>, r=200
 pos=<10,10,10>, r=5""".splitlines()
     nanobots = parse_input(example)
-
-
-
 
     print(part2(nanobots))
     # assert part2(nanobots) == 36
@@ -187,3 +184,16 @@ def test_part2():
     nanobots = parse_input(lines)
     print(part2(nanobots))
     # Too low: 127216436
+
+
+def test_part3():
+    lines = read_input()
+    nanobots = parse_input(lines)
+    point1 = (27_404_343, 159_381_107, 16_384_403, 120_279_732, 8_013_895, 11_802_623)
+    point2 = (27_404_343, 93_392_725, 16_384_403, 68_332_067, 8_013_895, 63_020_064)
+    n1 = [(i, n) for i, n in enumerate(nanobots) if n.is_cube_in_range(*point1)]
+    n2 = [(i, n) for i, n in enumerate(nanobots) if n.is_cube_in_range(*point2)]
+    print(len(n1), len(n2))
+
+    print(nanobots[1].is_cube_in_range(*point1))
+    print(nanobots[1].is_cube_in_range(*point2))
